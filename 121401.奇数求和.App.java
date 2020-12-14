c2ee068c304137e9ba26e77ffa52df2c7684f2e5
Swing1138 <74774045+Swing1138@users.noreply.github.com>
2020-12-14 15:53:38 +0800

    Create 121401.奇数求和.App.java

---
+++ 121401.奇数求和.App.java
@@ -0,0 +1,15 @@
+package java121401;
+public class App {
+public static void main(String[] args) {
+	long result = 0;
+
+    for(int i=1; i<2147483647; i=i+2) {
+
+           result = result + i;
+
+    }
+
+System.out.println("result=" + result);
+}
+
+}
